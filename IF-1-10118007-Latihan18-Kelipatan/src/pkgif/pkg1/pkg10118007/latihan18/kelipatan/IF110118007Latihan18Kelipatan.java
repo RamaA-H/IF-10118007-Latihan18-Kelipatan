566b6a97e665b737d4b7ee0b373cee6dda82a010
Rama Al Halik <55701022+RamaA-H@users.noreply.github.com>
2019-10-07 20:16:00 +0700

    Add files via upload

---
+++ IF-1-10118007-Latihan18-Kelipatan/src/pkgif/pkg1/pkg10118007/latihan18/kelipatan/IF110118007Latihan18Kelipatan.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkgif.pkg1.pkg10118007.latihan18.kelipatan;
+
+/**
+ * Nama      : Rama Al Halik
+ * Kelas     : IF-1
+ * NIM       : 10118007
+ * Deskripsi : Menampilkan kelipatan 3.5
+ * @author Rama
+ */
+public class IF110118007Latihan18Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+         float i;
+        for (i = (float) 3.5;i<=35;i+=3.5){
+            System.out.println(i+" "); 
+    }
+    }
+}
